1d94918fec36647237517f82ea60d961040fc7d6
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-03-29 21:13:41 +0300

    refactored consumer publisher inside UserNode

--- Distributed_System_part1/EventDeliverySystem/src/main/java/Distributed_System_part1/Nodes/UserNode.java
+++ Distributed_System_part1/EventDeliverySystem/src/main/java/Distributed_System_part1/Nodes/UserNode.java
@@ -1,10 +1,37 @@
 package Distributed_System_part1.Nodes;
 
 public class UserNode {
+    String username;
 
     public UserNode() {
-        //read username
-        // new thread publisher(username)
-        // new thread consumer(username)
+        //read and set username
+        // new thread publisher(username, this)
+        // new thread consumer(username, this)
+    }
+
+    private class Publisher implements Runnable{
+        UserNode parent;
+
+        public Publisher(UserNode parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    private class Consumer implements Runnable {
+        UserNode parent;
+
+        public Consumer(UserNode parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
