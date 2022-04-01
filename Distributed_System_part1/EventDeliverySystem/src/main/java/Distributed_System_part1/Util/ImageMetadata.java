aeab6cd171b1e7a6ccc8d0b2e3b2281b4a22f9ce
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-04-01 09:08:38 +0300

    added basic ImageMetadata, VideoMetadata members

--- Distributed_System_part1/EventDeliverySystem/src/main/java/Distributed_System_part1/Util/ImageMetadata.java
+++ Distributed_System_part1/EventDeliverySystem/src/main/java/Distributed_System_part1/Util/ImageMetadata.java
@@ -1,5 +1,42 @@
 package Distributed_System_part1.Util;
 
 public class ImageMetadata {
-    //TODO
+
+    private String fileName;
+    /**
+     * file size in bytes
+     */
+    private int fileSize;
+    /**
+     * width in pixels
+     */
+    private int width;
+    /**
+     * height in pixels
+     */
+    private int height;
+
+    public ImageMetadata(String fileName, int fileSize, int width, int height) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.width = width;
+        this.height = height;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
 }
--- Distributed_System_part1/EventDeliverySystem/src/main/java/Distributed_System_part1/Util/VideoMetadata.java
+++ Distributed_System_part1/EventDeliverySystem/src/main/java/Distributed_System_part1/Util/VideoMetadata.java
@@ -1,5 +1,59 @@
 package Distributed_System_part1.Util;
 
 public class VideoMetadata {
-    //TODO
+
+    private String fileName;
+    /**
+     * file size in bytes
+     */
+    private int fileSize;
+    /**
+     * frame width in pixels
+     */
+    private int frameWidth;
+    /**
+     * frame height in pixels
+     */
+    private int frameHeight;
+    /**
+     * video length in ms
+     */
+    private int length;
+    /**
+     * frames per second
+     */
+    private int frameRate;
+
+    public VideoMetadata(String fileName, int fileSize, int frameWidth, int frameHeight, int length, int frameRate) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.length = length;
+        this.frameRate = frameRate;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public int getFrameWidth() {
+        return frameWidth;
+    }
+
+    public int getFrameHeight() {
+        return frameHeight;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getFrameRate() {
+        return frameRate;
+    }
 }
