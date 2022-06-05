b9fafab161fc529657db8f5f4e0e222afde7318e
Vasilis Vlasopoulos <f3v3r.ath.cx@gmail.com>
2022-06-05 08:32:26 +0300

    added username dialog on connect

--- Distributed_System_part2/android/app/src/main/java/Distributed_System_part2/app/MainActivity.java
+++ Distributed_System_part2/android/app/src/main/java/Distributed_System_part2/app/MainActivity.java
@@ -20,7 +20,11 @@ import Distributed_System_part2.app.Node.UserNode;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String URL_BROKER1 = "10.0.2.2";
+    private final String URL_BROKER2 = "10.0.2.2";
+    private final String URL_BROKER3 = "10.0.2.2";
     private boolean connected = false;
+
     private Button connectButton;
     private Button newTopicButton;
     private Button refreshTopicsButton;
@@ -32,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connectButton = (Button)findViewById(R.id.connectButton);
-        newTopicButton = (Button)findViewById(R.id.newTopicButton);
-        refreshTopicsButton = (Button)findViewById(R.id.refreshTopicsButton);
-        topicsListView = (ListView)findViewById(R.id.topicsListView);
+        connectButton = (Button) findViewById(R.id.connectButton);
+        newTopicButton = (Button) findViewById(R.id.newTopicButton);
+        refreshTopicsButton = (Button) findViewById(R.id.refreshTopicsButton);
+        topicsListView = (ListView) findViewById(R.id.topicsListView);
     }
 
     @Override
@@ -61,81 +65,99 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //button = connect
-                if (!connected){
+                if (!connected) {
                     //TODO: request username and broker IPs, create new usernode
-                    //create usernode
-                    UserNode.createUserNodeInstance("username","10.0.2.2","10.0.2.2","10.0.2.2");
+                    AlertDialog.Builder connectDialog = new AlertDialog.Builder(MainActivity.this);
+                    final EditText usernameDialogEditText = new EditText(MainActivity.this);
+                    connectDialog.setTitle("Set username:");
+                    connectDialog.setView(usernameDialogEditText);
+                    connectDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
 
-                    //create topicsadapter, add callback on observable list topics
-                    topicsAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,UserNode.getUserNodeInstance().topics);
-                    UserNode.getUserNodeInstance().topics.addOnListChangedCallback(new ObservableList.OnListChangedCallback() {
-                        @Override
-                        public void onChanged(ObservableList sender) {
-                            runOnUiThread(new Runnable() {
+                            //create usernode
+                            UserNode.createUserNodeInstance(usernameDialogEditText.getText().toString(), URL_BROKER1, URL_BROKER2, URL_BROKER3);
+
+                            //create topicsadapter, add callback on observable list topics
+                            topicsAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, UserNode.getUserNodeInstance().topics);
+                            UserNode.getUserNodeInstance().topics.addOnListChangedCallback(new ObservableList.OnListChangedCallback() {
                                 @Override
-                                public void run() {
-                                    topicsAdapter.notifyDataSetChanged();
+                                public void onChanged(ObservableList sender) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            topicsAdapter.notifyDataSetChanged();
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onItemRangeChanged(ObservableList sender, int positionStart, int itemCount) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            topicsAdapter.notifyDataSetChanged();
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onItemRangeInserted(ObservableList sender, int positionStart, int itemCount) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            topicsAdapter.notifyDataSetChanged();
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onItemRangeMoved(ObservableList sender, int fromPosition, int toPosition, int itemCount) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            topicsAdapter.notifyDataSetChanged();
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onItemRangeRemoved(ObservableList sender, int positionStart, int itemCount) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            topicsAdapter.notifyDataSetChanged();
+                                        }
+                                    });
                                 }
                             });
-                        }
+                            //set adapter for topics list view
+                            topicsListView.setAdapter(topicsAdapter);
 
-                        @Override
-                        public void onItemRangeChanged(ObservableList sender, int positionStart, int itemCount) {
-                            runOnUiThread(new Runnable() {
+                            //set onclick listener for every item in list
+                            topicsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                                 @Override
-                                public void run() {
-                                    topicsAdapter.notifyDataSetChanged();
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                    Intent intent = new Intent(MainActivity.this, TopicActivity.class);
+                                    intent.putExtra("topic", adapterView.getItemAtPosition(i).toString()); // put the topic string in the intent extras
+                                    startActivity(intent);
                                 }
                             });
-                        }
 
-                        @Override
-                        public void onItemRangeInserted(ObservableList sender, int positionStart, int itemCount) {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    topicsAdapter.notifyDataSetChanged();
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onItemRangeMoved(ObservableList sender, int fromPosition, int toPosition, int itemCount) {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    topicsAdapter.notifyDataSetChanged();
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onItemRangeRemoved(ObservableList sender, int positionStart, int itemCount) {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    topicsAdapter.notifyDataSetChanged();
-                                }
-                            });
+                            refreshTopicsButton.setEnabled(true);
+                            newTopicButton.setEnabled(true);
+                            connectButton.setText(R.string.disconnect_button_text);
+                            connected = true;
                         }
                     });
-                    //set adapter for topics list view
-                    topicsListView.setAdapter(topicsAdapter);
-
-                    //set onclick listener for every item in list
-                    topicsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    connectDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                            Intent intent = new Intent(MainActivity.this, TopicActivity.class);
-                            intent.putExtra("topic",adapterView.getItemAtPosition(i).toString()); // put the topic string in the intent extras
-                            startActivity(intent);
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // do nothing if cancel
                         }
                     });
 
-                    refreshTopicsButton.setEnabled(true);
-                    newTopicButton.setEnabled(true);
-                    connectButton.setText(R.string.disconnect_button_text);
-                    connected = true;
+                    connectDialog.show();
+
                 } else { // button = disconnect
                     //TODO: disconnect, destroy usernode
                     UserNode.getUserNodeInstance().quit();
@@ -155,11 +177,12 @@ public class MainActivity extends AppCompatActivity {
                 //TODO: request new topic String, usernode.setTopic, go to TopicActivity with this topic string
                 AlertDialog.Builder setTopicDialog = new AlertDialog.Builder(MainActivity.this);
                 final EditText setTopicDialogEditText = new EditText(MainActivity.this);
+                setTopicDialog.setTitle("New Topic");
                 setTopicDialog.setView(setTopicDialogEditText);
                 setTopicDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
                         Intent intent = new Intent(MainActivity.this, TopicActivity.class);
-                        intent.putExtra("topic",setTopicDialogEditText.getText().toString()); // put the topic string in the intent extras
+                        intent.putExtra("topic", setTopicDialogEditText.getText().toString()); // put the topic string in the intent extras
                         startActivity(intent);
                     }
                 });
